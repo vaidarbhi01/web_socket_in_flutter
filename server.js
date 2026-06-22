553a82dbc05d8dbb1fbe236bd21ac5c27f5b6da7
Vaidarbhi <52734370+vaidarbhi01@users.noreply.github.com>
2026-06-22 12:24:39 +0530

    Added node server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  
+  ws.on('message', (message) => {
+    console.log('Received:', message.toString());
+    ws.send(message.toString()); // echo back
+  });
+
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+console.log('WebSocket server running on ws://localhost:8080');
